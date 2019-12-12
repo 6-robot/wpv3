5ffc4ab4e4f7873bbfc56460bb97318fe00ce1a4
Robot <zhangwanjie@126.com>
2019-12-12 10:34:36 +0800

    add battery state

--- wpv3_bringup/src/wpv3_core.cpp
+++ wpv3_bringup/src/wpv3_core.cpp
@@ -41,15 +41,22 @@
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Pose2D.h>
 #include <geometry_msgs/Twist.h>
+#include <sensor_msgs/BatteryState.h>
+#include <std_msgs/String.h>
 #include "WPV3_driver.h"
 #include <math.h>
 
+static float accLinear = 0.006; //线性移动加速度
+static float accAngular = 0.03; //旋转加速度
 static CWPV3_driver m_wpv3;
 static int nLastMotorPos[3];
+static geometry_msgs::Twist targetVel;
+static geometry_msgs::Twist curVel;
 void cmdVelCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
     //ROS_INFO("[m_wpv3] liner(%.2f %.2f) angular(%.2f)", msg->linear.x,msg->linear.y,msg->angular.z);
-    m_wpv3.Velocity(msg->linear.x,msg->linear.y,msg->angular.z);
+    //m_wpv3.Velocity(msg->linear.x,msg->linear.y,msg->angular.z);
+    targetVel = *msg;
 }
 
 static bool bFirstYaw = true;
@@ -76,6 +83,20 @@ void IMUCallback(const sensor_msgs::Imu::ConstPtr & imu)
      }
 }
  
+static geometry_msgs::Pose2D pose_diff_msg;
+void CtrlCallback(const std_msgs::String::ConstPtr &msg)
+{
+    int nFindIndex = 0;
+    nFindIndex = msg->data.find("pose_diff reset");
+    if( nFindIndex >= 0 )
+    {
+        pose_diff_msg.x = 0;
+        pose_diff_msg.y = 0;
+        pose_diff_msg.theta = 0;
+        //ROS_WARN("[pose_diff reset]");
+    }
+
+}
 
 static double fKVx = 1.0f/sqrt(3.0f);
 static double fKVy = 2.0f/3.0f;
@@ -138,9 +159,21 @@ int main(int argc, char** argv)
     odom.twist.twist.angular.y = 0;
     odom.twist.twist.angular.z = 0;
 
+    
+    ros::Subscriber ctrl_sub = n.subscribe("/wpv3/ctrl",10,&CtrlCallback);
+    ros::Publisher pose_diff_pub = n.advertise<geometry_msgs::Pose2D>("/wpv3/pose_diff",1);
+    pose_diff_msg.x = 0;
+    pose_diff_msg.y = 0;
+    pose_diff_msg.theta = 0;
+
+    targetVel.linear.x = targetVel.linear.y = targetVel.linear.z = targetVel.angular.x = targetVel.angular.y = targetVel.angular.z = 0;
+    curVel.linear.x = curVel.linear.y = curVel.linear.z = curVel.angular.x = curVel.angular.y = curVel.angular.z = 0;
     lastPose.x = lastPose.y = lastPose.theta = 0;
     lastVel.linear.x = lastVel.linear.y = lastVel.linear.z = lastVel.angular.x = lastVel.angular.y = lastVel.angular.z = 0;
     nLastMotorPos[0] = nLastMotorPos[1] = nLastMotorPos[2] = 0;
+
+    sensor_msgs::BatteryState  battery_msg;
+    ros::Publisher battery_pub = n.advertise<sensor_msgs::BatteryState>("/wpr1/battery_state",10);
 
     bool bFirst = true;
     while(n.ok())
@@ -218,6 +251,12 @@ int main(int argc, char** argv)
                 lastPose.theta = (fCurYaw - fYawZero);
             }
 
+            double pd_dx = (lastVel.linear.x*cos(pose_diff_msg.theta) - lastVel.linear.y*sin(pose_diff_msg.theta))*fTimeDur;
+            double pd_dy = (lastVel.linear.x*sin(pose_diff_msg.theta) + lastVel.linear.y*cos(pose_diff_msg.theta))*fTimeDur;
+            pose_diff_msg.x += pd_dx;
+            pose_diff_msg.y += pd_dy;
+            pose_diff_msg.theta += (fVz*fTimeDur);
+
             //ROS_INFO("[odom] x=%.2f y=%.2f a=%.2f",lastPose.x,lastPose.y,lastPose.theta);
 
             odom_quat = tf::createQuaternionMsgFromRollPitchYaw(0,0,lastPose.theta);
@@ -264,6 +303,39 @@ int main(int argc, char** argv)
             odom_pub.publish(odom);
             //ROS_INFO("[odom] zero");
         }
+
+
+        pose_diff_pub.publish(pose_diff_msg);
+        //ROS_INFO("[pose_diff_msg] x= %.2f  y=%.2f  th= %.2f", pose_diff_msg.x,pose_diff_msg.y,pose_diff_msg.theta);
+
+        ///////////////////////
+        // 加速度控制
+        if(targetVel.linear.x != curVel.linear.x)
+        {
+            float delta = targetVel.linear.x - curVel.linear.x;
+            if(delta > accLinear) delta = accLinear;
+            if(delta < -accLinear) delta = -accLinear;
+            curVel.linear.x += delta;
+        }
+        if(targetVel.linear.y != curVel.linear.y)
+        {
+            float delta = targetVel.linear.y - curVel.linear.y;
+            if(delta > accLinear) delta = accLinear;
+            if(delta < -accLinear) delta = -accLinear;
+            curVel.linear.y += delta;
+        }
+        if(targetVel.angular.z != curVel.angular.z)
+        {
+            float delta = targetVel.angular.z - curVel.angular.z;
+            if(delta > accAngular) delta = accAngular;
+            if(delta < -accAngular) delta = -accAngular;
+            curVel.angular.z += delta;
+        }
+        m_wpv3.Velocity(curVel.linear.x,curVel.linear.y,curVel.angular.z);
+        ///////////////////////
+        battery_msg.voltage = (float)m_wpv3.m_valData[4]*0.01;
+        battery_msg.charge = (m_wpv3.m_chIO & 0x04);
+        battery_pub.publish(battery_msg);
 
         ros::spinOnce();
         r.sleep();
