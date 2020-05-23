5e563f972fe68b95078334f75e330fcc0548806f
zhangwanjie <zhangwanjie@126.com>
2020-05-23 13:35:15 +0800

    update for melodic

--- wpv3_local_planner/src/wpv3_local_planner.cpp
+++ wpv3_local_planner/src/wpv3_local_planner.cpp
@@ -41,7 +41,7 @@
 #include <pluginlib/class_list_macros.h>
 
 // register this planner as a Wpv3LocalPlanner plugin
-PLUGINLIB_DECLARE_CLASS(wpv3_local_planner, Wpv3LocalPlanner, wpv3_local_planner::Wpv3LocalPlanner, nav_core::BaseLocalPlanner)
+PLUGINLIB_EXPORT_CLASS( wpv3_local_planner::Wpv3LocalPlanner, nav_core::BaseLocalPlanner)
 
 static float ranges[1081];
 namespace wpv3_local_planner
@@ -92,6 +92,44 @@ namespace wpv3_local_planner
         if(!m_bInitialized)
         {	
             m_tf_listener = tf;
+            m_costmap_ros = costmap_ros;
+            m_costmap = m_costmap_ros->getCostmap();
+            
+            m_global_frame_id = m_costmap_ros->getGlobalFrameID();      //"odom"
+            m_robot_base_frame_id = m_costmap_ros->getBaseFrameID();    //"base_footprint"
+            
+            m_footprint_spec = m_costmap_ros->getRobotFootprint();
+            costmap_2d::calculateMinAndMaxDistances(m_footprint_spec, m_robot_inscribed_radius, m_robot_circumscribed_radius); 
+
+            ros::NodeHandle nh_planner("~/" + name);
+            nh_planner.param("max_vel_trans", m_max_vel_trans, 1.0);
+            nh_planner.param("max_vel_rot", m_max_vel_rot, 0.9);
+            nh_planner.param("acc_scale_trans", m_acc_scale_trans, 1.5);
+            nh_planner.param("acc_scale_rot", m_acc_scale_rot, 0.6);
+            nh_planner.param("goal_dist_tolerance", m_goal_dist_tolerance, 0.2);
+            nh_planner.param("goal_yaw_tolerance", m_goal_yaw_tolerance, 0.03);
+            nh_planner.param("scan_topic", m_scan_topic, std::string("/scan"));
+
+            m_pub_target = nh_planner.advertise<geometry_msgs::PoseStamped>("local_planner_target", 10);
+            m_scan_sub = nh_planner.subscribe<sensor_msgs::LaserScan>(m_scan_topic,1,&Wpv3LocalPlanner::lidarCallback,this);
+            
+            m_bInitialized = true;
+
+            ROS_DEBUG("wpv3_local_planner plugin initialized.");
+        }
+        else
+        {
+            ROS_WARN("wpv3_local_planner has already been initialized, doing nothing.");
+        }
+    }
+
+
+    void Wpv3LocalPlanner::initialize(std::string name,tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* costmap_ros)
+    {
+        ROS_WARN("Wpv3LocalPlanner::initialize() ");
+        if(!m_bInitialized)
+        {	
+            m_tf_listener = new tf::TransformListener;
             m_costmap_ros = costmap_ros;
             m_costmap = m_costmap_ros->getCostmap();
             
